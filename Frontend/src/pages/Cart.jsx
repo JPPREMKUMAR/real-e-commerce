c81f0a41ae4ca702cc8c92fd95d1b49d6b3db5e4
JPPREMKUMAR <jppremkumar012@gmail.com>
2025-09-30 08:56:43 +0530

    newone

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import { leftStroke, plus, minus, deleteIcon } from "../assets/assets"
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { MainContext } from "../context/MainContext"
 
 
@@ -8,6 +8,19 @@ const Cart = () => {
 
 
     const { cartsList } = useContext(MainContext)
+    const [totalPrice, setTotalPrice] = useState(0)
+
+    useEffect(() => {
+
+        let newTotalPrice = 0
+        cartsList.map((item) => {
+
+            newTotalPrice += (item.price * item.quantity)
+        })
+        console.log(newTotalPrice)
+        setTotalPrice(newTotalPrice)
+
+    }, [])
 
     return (
 
@@ -118,14 +131,48 @@ const Cart = () => {
             {/* ToTal Price And Process Checkout */}
 
 
-            <div className="bg-[#F6F6F6] bg-cover w-full p-10 flex justify-end ">
+            <div className="bg-[#F6F6F6] bg-cover w-full px-20 py-10 flex justify-between pb-20">
 
                 <div>
-                    Part1
+
+
+                    <h1 className="text-[20px] font-bold text-[#3C4242]">Discount Codes</h1>
+
+                    <p className="text-[16px] text-[#807D7E]">Enter your coupon code if you have one</p>
+
+                    <div className="w-[380px] h-[43px] flex border-1 border-[#BEBCBD] rounded-lg mt-10 mb-10">
+                        <input type="text" className="px-3 outline-none bg-[#FFFFFF] rounded-l-lg" />
+                        <button className="w-[160px] h-full bg-[#8A33FD] text-[#FFFFFF] rounded-r-lg cursor-pointer">Apply Coupon</button>
+                    </div>
+                    <div>
+
+                        <button className="w-[232px] h-[43px] bg-[#FFFFFF] text-[16px] text-[#3C4242] rounded-lg border-1 border-[#BEBCBD]"> Continue Shopping</button>
+                    </div>
                 </div>
 
-                <div>
-                    Part2
+                <div className="bg-[#F3F3F3] bg-cover px-10 py-2 h-full">
+
+                    <div className="w-[300px]">
+                        <div className="flex justify-between items-center mb-2">
+                            <h1 className="text-[#3C4242] text-[22px]">Sub Total</h1>
+                            <h1 className="text-[#3C4242] text-[22px]">${totalPrice}.00</h1>
+                        </div>
+                        <div className="flex justify-between items-center">
+                            <h1 className="text-[#3C4242] text-[22px]">Shipping</h1>
+                            <h1 className="text-[#3C4242] text-[22px]">$5.00</h1>
+                        </div>
+                        <div className="flex justify-between items-center mb-5 mt-5" >
+                            <h1 className="text-gray-900 font text-[22px]">Grand Total</h1>
+                            <h1 className="text-gray-900 font text-[22px]">${totalPrice + 5}.00</h1>
+                        </div>
+                    </div>
+
+                    <hr className="text-[#BEBCBD] w-full mb-5" />
+                    <div className="flex justify-center items-center mt-10 mb-10">
+
+                        <button className="w-[232px] h-[46px] bg-[#8A33FD] text-[#FFFFFF] rounded-lg font-semibold">Proceed To Checkout</button>
+                    </div>
+
                 </div>
             </div>
 
